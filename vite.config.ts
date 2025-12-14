11d57407a9a102787ee8e678d629696ad7c8c426
midorin-Linux <midorin.shak12@gmail.com>
2025-12-14 15:10:44 +1300

    Add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,25 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        visualizer({
+            open: true, // Automatically open the report in your browser
+            filename: "dist/stats.html", // Output file
+        }),
+    ],
     build: {
         outDir: "static",
         emptyOutDir: true,
     },
     resolve: {
         alias: {
-        "@": path.resolve(__dirname, "./ts-src"),
+            "@": path.resolve(__dirname, "./ts-src"),
         },
     },
 });
