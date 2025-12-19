2c955674c31ef1c49a49ab0c840b36b9cad54a0f
midorin-Linux <midorin.shak12@gmail.com>
2025-12-19 11:21:39 +0900

    Refactor configuration files and update default settings

--- agent/src/app/runner.rs
+++ agent/src/app/runner.rs
@@ -6,15 +6,19 @@ use std::{
 
 use anyhow::{Context, Result};
 use axum::{
+    extract::ConnectInfo,
+    middleware::Next,
+    response::{IntoResponse, Response},
     Router,
     routing::get,
     http::StatusCode,
 };
 use indicatif::{ProgressBar, ProgressStyle};
 use owo_colors::OwoColorize;
+use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
 use tower_http::{timeout::TimeoutLayer, trace::TraceLayer};
-use tracing::info;
+use tracing::{debug, error, info, warn};
 
 pub struct App {
     config: Config,
@@ -44,6 +48,7 @@ impl App {
             .layer((
                 TraceLayer::new_for_http(),
                 TimeoutLayer::with_status_code(StatusCode::REQUEST_TIMEOUT, Duration::from_secs(10)),
+                axum::middleware::from_fn(whitelist)
             ));
 
         let listener =
@@ -52,7 +57,10 @@ impl App {
         pb.finish_and_clear();
         println!("{}", format!("{} Ready!\n", "✔".green()));
 
-        axum::serve(listener, app)
+        axum::serve(
+            listener,
+            app.into_make_service_with_connect_info::<SocketAddr>()
+        )
             .with_graceful_shutdown(shutdown_signal())
             .await
             .context("failed to start server")?;
@@ -60,4 +68,41 @@ impl App {
         info!("Server shutting down gracefully.");
         Ok(())
     }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Whitelist {
+    ip_address: String,
+}
+
+async fn whitelist(
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    request: axum::extract::Request,
+    next: Next,
+) -> Response {
+    debug!("Request from: {}", addr.ip());
+
+    let json_row = match std::fs::read_to_string("whitelist.json") {
+        Ok(s) => s,
+        Err(e) => {
+            error!("Failed to read whitelist.json: {}", e);
+            return (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error").into_response();
+        }
+    };
+
+    let whitelist: Vec<Whitelist> = match serde_json::from_str(&json_row) {
+        Ok(json) => json,
+        Err(e) => {
+            error!("Failed to parse whitelist.json: {}", e);
+            return (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error").into_response();
+        }
+    };
+
+    let client_ip = addr.ip().to_string();
+    if !whitelist.iter().any(|data| data.ip_address.eq(&client_ip)) {
+        warn!("Access denied for IP: {}", client_ip);
+        return (StatusCode::FORBIDDEN, "Forbidden").into_response();
+    }
+
+    next.run(request).await
 }
